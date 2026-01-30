4e5d8c5c1f4e468edadda7a3541f76767f56b2a9
Bolu <108519875+Bolu-Adekanmbi@users.noreply.github.com>
2026-01-29 21:46:18 -0500

    Changes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('dist'));
 
 let notes = [
   {
@@ -87,3 +88,4 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
