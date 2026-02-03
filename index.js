73b58e9cfc627b2caf22ceb55881ed34274fde81
Bolu <108519875+Bolu-Adekanmbi@users.noreply.github.com>
2026-02-03 15:26:18 -0500

    Finished First Section of Error Handling

--- index.js
+++ index.js
@@ -72,9 +72,18 @@ app.post('/api/notes', (request, response) => {
 
 app.get('/api/notes/:id', (request, response) => {
   const id = request.params.id;
-  Note.findById(id).then(note => {
-    response.json(note);
-  })
+  Note.findById(id)
+    .then(note => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      response.status(400).json({error: "Malformatted ID"});
+    })
 });
 
 const PORT = process.env.PORT;
