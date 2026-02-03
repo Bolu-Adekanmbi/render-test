9ead04c1fcaf55a47613468af5a9568367d5f480
Bolu <108519875+Bolu-Adekanmbi@users.noreply.github.com>
2026-02-03 14:59:03 -0500

    Removed Mongoose to its own separate module

--- index.js
+++ index.js
@@ -1,33 +1,10 @@
+require('dotenv').config();
+const Note = require('./models/note');
 const express = require('express');
 const app = express();
-const cors = require('cors');
-
-const mongoose = require('mongoose');
-
-// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
-const password = process.argv[2]
-const url = `mongodb+srv://fullstack:${password}@cluster0.izoedye.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
-
-mongoose.set('strictQuery',false)
-mongoose.connect(url, { family: 4 })
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Note = mongoose.model('Note', noteSchema)
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 app.use(express.static('dist'));
 
 let notes = [
@@ -81,36 +58,26 @@ app.post('/api/notes', (request, response) => {
     });
   }
 
-  const newNote = {
+  const newNote = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId()
-  };
+  });
 
-  notes = notes.concat(newNote);
+  newNote.save().then(savedNote => {
+    console.log(`The note "${newNote.content}" was saved!`);
+    response.json(savedNote);
+  });
 
-  // Debugging statements
-  console.log("New note:", newNote);
-  console.log();
-  console.log("Content Type:", request.get("content-type")); //getting content type - v1
-  console.log(request.headers); //getting all headers - v2
-
-  response.json(newNote);
 });
 
 app.get('/api/notes/:id', (request, response) => {
   const id = request.params.id;
-  const note = notes.find(n => n.id === id);
-
-  if (note) {
+  Note.findById(id).then(note => {
     response.json(note);
-  } else {
-    response.statusMessage = `The note with id: "${id}" cannot be found :( !`;
-    response.status(404).end();
-  }
+  })
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
